5bca03737479f4e9a05ca910fd0af69a0aff1e92
Greg Pfeil <greg@technomadic.org>
2010-05-07 08:26:30 -0400

    Got rid of the one error (redefining 'match') that occured when loading.

--- package.lisp
+++ package.lisp
@@ -3,6 +3,7 @@
    "A simple and direct implementation of the Kell calculus, suitable for testing
     the behavior of more advanced implementations.")
   (:use #:cl #:bordeaux-threads #:unify)
+  (:shadow #:match)
   (:export #:trigger
            #:restriction
            #:message
