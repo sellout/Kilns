bf1ea868c078358a0250cdef6c95beddf2aac817
Greg Pfeil <greg@technomadic.org>
2010-10-12 18:13:44 -0400

    Didn’t test the previous commit very well. This fixes some issues. Now you can run `./kilns -c 2` to start it on a 2-core machine.

--- build-application.lisp
+++ build-application.lisp
@@ -4,10 +4,10 @@
 (asdf:load-system :kilns)
 
 (defun application-toplevel ()
-  (destructuring-bind (app &optional cpu-count kell)
+  (destructuring-bind (app &optional cpu-tag cpu-count kell)
                       (ccl::command-line-arguments)
-    (declare (ignore app))
-    (kilns::toplevel (parse-integer cpu-count) kell)))
+    (declare (ignore app cpu-tag))
+    (kilns::toplevel (if cpu-count (parse-integer cpu-count)) kell)))
 
 (save-application "kilns"
                   :toplevel-function #'application-toplevel :prepend-kernel t)
