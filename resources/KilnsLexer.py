603261afc0efde422ae1081f49e59c8f868dfd99
Greg Pfeil <greg@technomadic.org>
2010-10-13 11:29:19 -0400

    Here's a Pygments lexer for Kilns. It may not work, but it's something.

---
+++ resources/KilnsLexer.py
@@ -0,0 +1,16 @@
+from pygments.lexer import RegexLexer
+from pygments.token import *
+
+class DiffLexer(RegexLexer):
+    name = 'Kilns'
+    aliases = ['kilns']
+    filenames = ['*.kiln']
+
+    tokens = {
+        'root': [
+            (r';.*?$', Comment),
+            (r'".*?"', String),
+            (r'trigger|def|par|new|null-process', Keyword),
+            (r'?\w+', Variable),
+        ]
+    }
