364ae8ae0a8f02edd6b253807445a007aed29c25
Greg Pfeil <greg@technomadic.org>
2011-02-12 12:41:04 -0500

    Use CLON for command-line processing, which gives us a lot more flexibility.

--- build/build-application.lisp
+++ build/build-application.lisp
@@ -11,12 +11,32 @@
 
 (load "kilns.asd")
 (ql:quickload "kilns" :verbose nil)
+(ql:quickload "com.dvlsoft.clon" :verbose nil)
+
+(com.dvlsoft.clon:defsynopsis (#|:postfix "FILES..."|#)
+  (text :contents "The Kilns programming language, based on the Kell calculus.")
+  (group (:header "Immediate exit options:")
+         (flag :short-name "h" :long-name "help"
+               :description "Print this help and exit.")
+         (flag :short-name "v" :long-name "version"
+               :description "Print version number and exit."))
+  (lispobj :short-name "c" :long-name "cpu-count"
+           :argument-name "NUMBER" :typespec '(integer 1)
+           :description "The number of CPUs/cores available.")
+  (lispobj :short-name "k" :long-name "kell"
+           :argument-name "NAME" :typespec 'symbol
+           :description "The name of the kell to use as the top kell."))
 
 (defun application-toplevel ()
-  (destructuring-bind (app &optional cpu-tag cpu-count kell)
-                      (ccl::command-line-arguments)
-    (declare (ignore app cpu-tag))
-    (kilns::toplevel (if cpu-count (parse-integer cpu-count)) kell)))
+  (com.dvlsoft.clon:make-context)
+  (cond ((com.dvlsoft.clon:getopt :long-name "help")
+         (com.dvlsoft.clon:help)
+         (com.dvlsoft.clon:exit))
+        ((com.dvlsoft.clon:getopt :long-name "version")
+         (format t "kilns version ~a"
+                 (asdf:component-version (asdf:find-system :kilns)))
+         (com.dvlsoft.clon:exit)))
+  (kilns::toplevel (com.dvlsoft.clon:getopt :long-name "cpu-count")
+                   (com.dvlsoft.clon:getopt :long-name "kell")))
 
-(save-application "kilns"
-                  :toplevel-function #'application-toplevel :prepend-kernel t)
+(com.dvlsoft.clon:dump "kilns" application-toplevel)
