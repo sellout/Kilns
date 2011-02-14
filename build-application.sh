ee268bd05cca0e5ed16d7c31545d161d10746bb5
Greg Pfeil <greg@technomadic.org>
2011-02-14 10:20:31 -0500

    Can now have build-application run the implementation (fiveam) tests by giving it a parameter.

--- build-application.sh
+++ build-application.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 dx86cl64 --load "build/build-application"
+
+if [ $# -gt 0 ]; then
+   # the QUICKLOAD is for quieter system loading
+   dx86cl64 --eval '(load "kilns.asd")' \
+            --eval '(ql:quickload "kilns")' \
+            --eval '(asdf:test-system :kilns)'
+fi
