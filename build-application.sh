c78c0d4cd7243634813f55a6efd54648a13e071f
Greg Pfeil <greg@technomadic.org>
2011-02-14 11:13:05 -0500

    Whoops, was missing a line continuation.

--- build-application.sh
+++ build-application.sh
@@ -6,6 +6,6 @@ if [ $# -gt 0 ]; then
    # the QUICKLOAD is for quieter system loading
    dx86cl64 --eval '(load "kilns.asd")' \
             --eval '(ql:quickload "kilns")' \
-            --eval '(asdf:test-system :kilns)'
+            --eval '(asdf:test-system :kilns)' \
             --eval '(quit)'
 fi
