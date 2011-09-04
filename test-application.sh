834b6399e0b2e7a724ac80d53f47a38d59ede373
Greg Pfeil <greg@technomadic.org>
2011-09-04 14:29:37 -0600

    Make `test-application.sh` a bit more robust and clear.

--- test-application.sh
+++ test-application.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
+if [ -f './kilns' ]; then
+   rm ./kilns
+fi
+
 ./build-application.sh test-implementation
-./kilns -c ${1:-2} tests/run-tests.kiln
+
+if [ -f './kilns' ]; then
+   ./kilns -c ${1:-2} tests/run-tests.kiln
+else
+   echo '`kilns` does not exist'
+fi
+
+
