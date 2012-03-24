7dd52a7ae97b27ad7f2d0c2efe9a9d1cfb2400e3
Greg Pfeil <greg@technomadic.org>
2012-03-23 22:15:58 -0600

    Simplify `SET=`.

--- src/utilities.lisp
+++ src/utilities.lisp
@@ -4,8 +4,7 @@
 ;;; Definitions for things assumed to exist by the Kell calculus
 
 (defun set= (left right)
-  (and (null (set-difference left right))
-       (null (set-difference right left))))
+  (null (set-exclusive-or left right)))
 
 (defun mappend (fn list)
   (apply #'append (mapcar fn list)))
